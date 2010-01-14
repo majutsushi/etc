f4846b8f032f9e6234370aa612a4193bb6962432
Jan Larres <jan@majutsushi.net>
2010-01-14 16:22:12 +1300

    xmonad: add proper fullscreen support for some applications

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -26,6 +26,8 @@ import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run(spawnPipe)
+import Data.Monoid (All (All))
+import Monad
 import System.IO
 import Text.Regex.Posix
 
@@ -47,7 +49,8 @@ myConfig = gnomeConfig {
                             <+> namedScratchpadManageHook scratchpads
                             <+> manageDocks
                             <+> manageHook gnomeConfig,
-               layoutHook = myLayoutHook
+               layoutHook = myLayoutHook,
+               handleEventHook = evHook
            } `additionalKeys` [
 --              ((0, xK_Print), spawn "scrot"),
                ((mod4Mask, xK_a), sendMessage MirrorShrink),
@@ -220,3 +223,38 @@ myDzenPP h = defaultPP {
                  ppOutput = hPutStrLn h
              }
 
+-- from http://code.google.com/p/xmonad/issues/detail?id=339
+-- Helper functions to fullscreen the window
+fullFloat, tileWin :: Window -> X ()
+fullFloat w = windows $ W.float w r
+    where r = W.RationalRect 0 0 1 1
+tileWin w = windows $ W.sink w
+
+evHook :: Event -> X All
+evHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
+  state <- getAtom "_NET_WM_STATE"
+  fullsc <- getAtom "_NET_WM_STATE_FULLSCREEN"
+  isFull <- runQuery isFullscreen win
+
+  -- Constants for the _NET_WM_STATE protocol
+  let remove = 0
+      add = 1
+      toggle = 2
+
+      -- The ATOM property type for changeProperty
+      ptype = 4
+
+      action = head dat
+
+  when (typ == state && (fromIntegral fullsc) `elem` tail dat) $ do
+    when (action == add || (action == toggle && not isFull)) $ do
+         io $ changeProperty32 dpy win state ptype propModeReplace [fromIntegral fullsc]
+         fullFloat win
+    when (head dat == remove || (action == toggle && isFull)) $ do
+         io $ changeProperty32 dpy win state ptype propModeReplace []
+         tileWin win
+
+  -- It shouldn't be necessary for xmonad to do anything more with this event
+  return $ All False
+
+evHook _ = return $ All True
