11d8998043f81da43ec475e1099517161c2445af
Jan Larres <jan@majutsushi.net>
2012-02-01 18:59:39 +1300

    add installation script

---
+++ bin/etcinstall.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+xlink() {
+    if [[ -a "$2" && ! -L "$2" ]]; then
+        echo -e "\033[31m$2 is not a symlink!\033[0m"
+    else
+        echo "Linking "$2""
+        rm "$2"
+        ln -s "$1" "$2"
+    fi
+}
+
+OLDPWD=$PWD
+cd $HOME
+
+xlink .etc/emacs/emacs   .emacs
+xlink .etc/emacs/emacs.d .emacs.d
+
+xlink .etc/git/gitconfig .gitconfig
+
+xlink .etc/mercurial/hgrc .hgrc
+
+if command mocp >/dev/null 2>&1; then
+    xlink .etc/moc .moc
+fi
+
+xlink .etc/mutt/muttrc .muttrc
+xlink .etc/mutt/mutt   .mutt
+
+xlink .etc/pentadactyl/pentadactylrc .pentadactylrc
+xlink .etc/pentadactyl/pentadactyl   .pentadactyl
+
+xlink .etc/screen/screenrc .screenrc
+
+xlink .etc/slrn/slrnrc .slrnrc
+
+xlink .etc/vim/vimrc      .vimrc
+xlink .etc/vim/vim        .vim
+xlink .etc/vim/vimplaterc .vimplaterc
+
+if command xmonad >/dev/null 2>&1; then
+    xlink .etc/xmonad .xmonad
+fi
+
+#xlink .etc/xorg/Xmodmap    .Xmodmap
+#xlink .etc/xorg/Xresources .Xresources
+#xlink .etc/xorg/xinitrc    .xinitrc
+#xlink .etc/xorg/xinput.d   .xinput.d
+#xlink .etc/xorg/xinputrc   .xinputrc
+#xlink .etc/xorg/xsession   .xsession
+#xlink .etc/xorg/xsessionrc .xsessionrc
+
+xlink .etc/zsh/zshenv .zshenv
+
+xlink .etc/Rprofile   .Rprofile
+xlink .etc/bibtoolrsc .bibtoolrsc
+xlink .etc/colorgccrc .colorgccrc
+xlink .etc/ctags      .ctags
+xlink .etc/gdbinit    .gdbinit
+xlink .etc/indent.pro .indent.pro
+xlink .etc/irbrc      .irbrc
+xlink .etc/lessfilter .lessfilter
+#xlink .etc/mailcap    .mailcap
+#xlink .etc/procmailrc .procmailrc
+
+cd $OLDPWD
