54b3edfc091dca08554264d08bcf7a95206eb87c
Jan Larres <jan@majutsushi.net>
2012-09-08 21:26:26 +1200

    mail: only scan sent mails for addresses

--- mail/refreshaddresses.sh
+++ mail/refreshaddresses.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set up an array with outboxes
-sentboxes=( "~/Maildir/cur" "~/Maildir/.Archive" "~/Maildir/.sent" )
-# sentboxes=( "~/Maildir/.sent" )
+# sentboxes=( "~/Maildir/cur" "~/Maildir/.Archive" "~/Maildir/.sent" )
+sentboxes=( "~/Maildir/.sent" )
 
 # Run mu on each of them
 for dir in "${sentboxes[@]}"
