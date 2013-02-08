ffe9a224b4b3b810c7f281e6537145271bd85727
Jan Larres <jan@majutsushi.net>
2013-02-08 14:49:54 +1300

    ranger: use unicode ellipsis

--- ranger/options.py
+++ ranger/options.py
@@ -38,7 +38,7 @@ preview_script = '~/.config/ranger/scope.sh'
 use_preview_script = True
 
 # Use a unicode "..." character to mark cut-off filenames?
-unicode_ellipsis = False
+unicode_ellipsis = True
 
 # Show dotfiles in the bookmark preview box?
 show_hidden_bookmarks = True
