492917167e6e4b64317bb480fae81c27f8c3dd71
Jan Larres <jan@majutsushi.net>
2014-01-19 15:16:45 +1300

    awesome: add some utility functions

--- awesome/eldritch/utils.lua
+++ awesome/eldritch/utils.lua
@@ -4,6 +4,33 @@ local osd = require("eldritch.osd")
 
 local utils = {}
 
+function utils.exec(cmd)
+    local f = io.popen(cmd)
+    local out = f:read("*all")
+    f:close()
+    return out
+end
+
+function utils.split(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+
+    local t = {}
+    local i = 1
+
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        t[i] = str
+        i = i + 1
+    end
+
+    return t
+end
+
+function utils.trim(s)
+    return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
 function utils.brightness(change)
     awful.util.pread("xbacklight -inc " .. change)
     local value = math.floor(awful.util.pread("xbacklight -get"))
@@ -12,16 +39,16 @@ function utils.brightness(change)
 end
 
 function utils.font(font, text)
-  return '<span font="' .. tostring(font) .. '">' .. tostring(text) ..'</span>'
+    return '<span font="' .. tostring(font) .. '">' .. tostring(text) ..'</span>'
 end
 function utils.fgcolor(color, text)
-  return '<span fgcolor="' .. tostring(color) .. '">' .. tostring(text) .. '</span>'
+    return '<span fgcolor="' .. tostring(color) .. '">' .. tostring(text) .. '</span>'
 end
 function utils.bgcolor(color, text)
-  return '<span bgcolor="' .. tostring(color) .. '">' .. tostring(text) .. '</span>'
+    return '<span bgcolor="' .. tostring(color) .. '">' .. tostring(text) .. '</span>'
 end
 function utils.bold(text)
-  return '<span font_weight="bold">' .. tostring(text) .. '</span>'
+    return '<span font_weight="bold">' .. tostring(text) .. '</span>'
 end
 
 return utils
