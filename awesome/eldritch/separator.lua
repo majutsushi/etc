d8234f74161685a9aa005f9a4e32f91535f11d40
Jan Larres <jan@majutsushi.net>
2014-03-03 00:56:03 +1300

    awesome: first stab at proper separator

---
+++ awesome/eldritch/separator.lua
@@ -0,0 +1,14 @@
+local separator = wibox.widget.base.make_widget()
+
+function separator.fit(sep, width, height)
+    return width, 10
+end
+
+function separator.draw(sep, wibox, cr, width, height)
+    cr:move_to(width / 4, height / 2)
+    cr:line_to(width * 3 / 4, height / 2)
+    cr:set_line_width(3)
+    cr:stroke()
+end
+
+return separator
