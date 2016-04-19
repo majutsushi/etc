1b570a947e2fac2e9ecf27fba63f77ba03c43095
Jan Larres <jan@majutsushi.net>
2016-04-19 12:00:27 +1200

    mail: use new server settings

--- mail/offlineimap-helpers.py
+++ mail/offlineimap-helpers.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
-import gnomekeyring as gkey
+import keyring
 
-def get_username(server):
-    pwdata = gkey.find_network_password_sync(server = server, protocol = 'imap')[0]
-    return pwdata['user']
-
-def get_password(server):
-    pwdata = gkey.find_network_password_sync(server = server, protocol = 'imap')[0]
-    return pwdata['password']
+def get_password():
+    return keyring.get_password("majutsushi.net", "jan")
