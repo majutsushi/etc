92731b51697b3434a009bbbea9a274b2ca787539
Jan Larres <jan@majutsushi.net>
2016-10-05 17:01:12 +1300

    awesome,xorg: replace kupfer with rofi

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -610,6 +610,7 @@ globalkeys = awful.util.table.join(
     awful.key({}, "XF86MonBrightnessUp", function () eldritch.utils.brightness(4) end),
     awful.key({}, "XF86MonBrightnessDown", function () eldritch.utils.brightness(-4) end),
 
+    awful.key({ modkey }, "p", function () exec("rofi -show drun") end),
     awful.key({ altkey, "Control" }, "l", function () exec("slock") end),
     awful.key({ },           "Print", function () exec("gnome-screenshot") end),
     awful.key({ "Control" }, "Print", function () exec("gnome-screenshot --interactive") end),
