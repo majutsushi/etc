5e5e3a4d4d0d3103efc35d6abb76e2228abcef98
Jan Larres <jan@majutsushi.net>
2017-03-06 18:15:19 +1300

    awesome: fix tasklist tooltips

--- awesome/eldritch/utils.lua
+++ awesome/eldritch/utils.lua
@@ -1,6 +1,9 @@
 local awful = require("awful")
+awful.widget.common = require("awful.widget.common")
+local beautiful = require("beautiful")
 local naughty = require("naughty")
 local osd = require("eldritch.osd")
+local wibox = require("wibox")
 
 local utils = {}
 
@@ -88,6 +91,82 @@ function utils.fullscreens(c)
     end
     -- focus our client
     client.focus = c
+end
+
+function utils.tasklist_update(w, buttons, label, data, objects)
+    local dpi = beautiful.xresources.apply_dpi
+
+    -- update the widgets, creating them if needed
+    w:reset()
+    for i, o in ipairs(objects) do
+        local cache = data[o]
+        local ib, tb, bgb, tbm, ibm, l, tt
+        if cache then
+            ib = cache.ib
+            tb = cache.tb
+            bgb = cache.bgb
+            tbm = cache.tbm
+            ibm = cache.ibm
+            tt = cache.tt
+        else
+            ib = wibox.widget.imagebox()
+            tb = wibox.widget.textbox()
+            bgb = wibox.container.background()
+            tbm = wibox.container.margin(tb, dpi(4), dpi(4))
+            ibm = wibox.container.margin(ib, dpi(4))
+            l = wibox.layout.fixed.horizontal()
+            tt = awful.tooltip({ objects = { l } })
+
+            -- All of this is added in a fixed widget
+            l:fill_space(true)
+            l:add(ibm)
+            l:add(tbm)
+
+            -- And all of this gets a background
+            bgb:set_widget(l)
+
+            bgb:buttons(awful.widget.common.create_buttons(buttons, o))
+
+            data[o] = {
+                ib  = ib,
+                tb  = tb,
+                bgb = bgb,
+                tbm = tbm,
+                ibm = ibm,
+                tt  = tt,
+            }
+        end
+
+        local text, bg, bg_image, icon, args = label(o, tb)
+        args = args or {}
+
+        -- The text might be invalid, so use pcall.
+        if text == nil or text == "" then
+            tbm:set_margins(0)
+        else
+            if not tb:set_markup_silently(text) then
+                tb:set_markup("<i>&lt;Invalid text&gt;</i>")
+            end
+        end
+        bgb:set_bg(bg)
+        if type(bg_image) == "function" then
+            -- TODO: Why does this pass nil as an argument?
+            bg_image = bg_image(tb,o,nil,objects,i)
+        end
+        bgb:set_bgimage(bg_image)
+        if icon then
+            ib:set_image(icon)
+        else
+            ibm:set_margins(0)
+        end
+        tt:set_markup(text)
+
+        bgb.shape              = args.shape
+        bgb.shape_border_width = args.shape_border_width
+        bgb.shape_border_color = args.shape_border_color
+
+        w:add(bgb)
+    end
 end
 
 return utils
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -243,7 +243,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons, {}, tasklist_update)
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons, {}, eldritch.utils.tasklist_update)
 
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", height = 22, screen = s })
@@ -274,80 +274,6 @@ awful.screen.connect_for_each_screen(function(s)
         },
     }
 end)
-
-function tasklist_update(w, buttons, label, data, objects)
-    -- update the widgets, creating them if needed
-    w:reset()
-    for i, o in ipairs(objects) do
-        local cache = data[o]
-        local ib, tb, bgb, tbm, ibm, l, tt
-        if cache then
-            ib = cache.ib
-            tb = cache.tb
-            bgb = cache.bgb
-            tbm = cache.tbm
-            ibm = cache.ibm
-            tt = cache.tt
-        else
-            ib = wibox.widget.imagebox()
-            tb = wibox.widget.textbox()
-            bgb = wibox.container.background()
-            tbm = wibox.container.margin(tb, dpi(4), dpi(4))
-            ibm = wibox.container.margin(ib, dpi(4))
-            l = wibox.layout.fixed.horizontal()
-            tt = awful.tooltip({ objects = { l } })
-
-            -- All of this is added in a fixed widget
-            l:fill_space(true)
-            l:add(ibm)
-            l:add(tbm)
-
-            -- And all of this gets a background
-            bgb:set_widget(l)
-
-            bgb:buttons(common.create_buttons(buttons, o))
-
-            data[o] = {
-                ib  = ib,
-                tb  = tb,
-                bgb = bgb,
-                tbm = tbm,
-                ibm = ibm,
-                tt  = tt,
-            }
-        end
-
-        local text, bg, bg_image, icon, args = label(o, tb)
-        args = args or {}
-
-        -- The text might be invalid, so use pcall.
-        if text == nil or text == "" then
-            tbm:set_margins(0)
-        else
-            if not tb:set_markup_silently(text) then
-                tb:set_markup("<i>&lt;Invalid text&gt;</i>")
-            end
-        end
-        bgb:set_bg(bg)
-        if type(bg_image) == "function" then
-            -- TODO: Why does this pass nil as an argument?
-            bg_image = bg_image(tb,o,nil,objects,i)
-        end
-        bgb:set_bgimage(bg_image)
-        if icon then
-            ib:set_image(icon)
-        else
-            ibm:set_margins(0)
-        end
-        tt:set_markup(text)
-
-        bgb.shape              = args.shape
-        bgb.shape_border_width = args.shape_border_width
-        bgb.shape_border_color = args.shape_border_color
-
-        w:add(bgb)
-    end
-end
 -- }}}
 
 -- {{{ Mouse bindings
