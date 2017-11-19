a2a9f47634b1bf46c44a27c3d7fc0b16801331b9
Jan Larres <jan@majutsushi.net>
2017-11-19 18:58:02 +1300

    awesome: don't switch tags with scroll wheel

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -285,9 +285,7 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 3, function () mymainmenu:toggle() end)
 ))
 -- }}}
 
