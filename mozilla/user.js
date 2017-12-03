68fcdcfe2bea550885d95c4b20062f1dff68a2b5
Jan Larres <jan@majutsushi.net>
2017-12-03 16:30:47 +1300

    add mozilla config files

---
+++ mozilla/user.js
@@ -0,0 +1,28 @@
+// Open all requests for new windows in new tabs instead
+user_pref('browser.link.open_newwindow', 3);
+user_pref('browser.link.open_newwindow.restriction', 0);
+
+// Resume previous session on startup
+user_pref('browser.startup.page', 3);
+
+user_pref('browser.tabs.loadDivertedInBackground', true);
+user_pref('browser.urlbar.trimURLs', false);
+user_pref('devtools.chrome.enabled', true);
+user_pref('devtools.command-button-eyedropper.enabled', true);
+user_pref('devtools.command-button-rulers.enabled', true);
+user_pref('devtools.theme', 'dark');
+user_pref('devtools.selfxss.count', 0);
+user_pref('dom.event.clipboardevents.enabled', false);
+user_pref('general.warnOnAboutConfig', false);
+user_pref('media.autoplay.enabled', false);
+user_pref('network.cookie.cookieBehavior', 1);
+// user_pref('network.dns.disablePrefetch', true);
+// user_pref('network.http.speculative-parallel-limit', 0);
+user_pref('network.http.referer.trimmingPolicy', 2);
+user_pref('network.http.referer.XOriginPolicy', 1);
+user_pref('network.http.referer.XOriginTrimmingPolicy', 2);
+// user_pref('network.prefetch-next', false);
+// user_pref('noscript.clearClick.rapidFireCheck', false);
+user_pref('privacy.donottrackheader.enabled', true);
+user_pref('privacy.donottrackheader.value', 1);
+user_pref('privacy.trackingprotection.enabled', true);
