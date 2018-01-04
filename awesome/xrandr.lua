bc23baeb0bfa59f74e511e9eee11f1fa19246db4
Jan Larres <jan@majutsushi.net>
2018-01-04 22:00:05 +1300

    awesome: missed Lua 5.3 fix

--- awesome/xrandr.lua
+++ awesome/xrandr.lua
@@ -174,7 +174,7 @@ local function xrandr()
         label = "Keep the current configuration"
         state.index = nil
     else
-        label, action = unpack(next)
+        label, action = table.unpack(next)
     end
     state.cid = naughty.notify({ text = label,
                                  icon = icon_path,
