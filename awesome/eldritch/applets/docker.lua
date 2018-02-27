5b846bd996a8b4850bf489b3ba2d68b587fe3fb8
Jan Larres <jan@majutsushi.net>
2018-02-27 13:45:25 +1300

    awesome: improve docker tooltip

--- awesome/eldritch/applets/docker.lua
+++ awesome/eldritch/applets/docker.lua
@@ -7,6 +7,33 @@ local workers = require("eldritch.workers")
 
 local dockerwidget = {}
 
+local imagecolor = beautiful.tooltip_title_color or "#f0e68c"
+local headercolor = beautiful.tooltip_key_color or "#98fb98"
+
+local function gettags(image)
+    local f = io.popen("docker image inspect --format='{{.RepoTags}}' " .. image)
+
+    local tags = f:read()
+
+    f:close()
+
+    tags = tags:sub(2, -2)
+
+    local rv = {}
+
+    for _, tag in pairs(utils.split(tags, " ")) do
+        if tag:find("amazonaws.com") then
+            local slash = tag:find("/")
+            if slash then
+                tag = "ECR" .. tag:sub(slash)
+            end
+        end
+        rv[#rv + 1] = tag
+    end
+
+    return rv
+end
+
 local function new()
     local icon = wibox.widget.imagebox(beautiful.widget_docker)
 
@@ -18,15 +45,42 @@ local function new()
 
     icon.tooltip = awful.tooltip({ objects = { widget } })
 
+    function icon.tooltip.update()
+        local f = io.popen("docker ps --format='{{.Image}}\t{{.Command}}\t{{.Names}}' --no-trunc")
+
+        local data = {}
+
+        for line in f:lines() do
+            local linedata = utils.split(line, "\t")
+
+            local image = linedata[1]
+            local command = linedata[2]
+            local names = linedata[3]
+
+            local tags = gettags(image)
+
+            local text = utils.bold(utils.fgcolor(imagecolor, image))
+            text = text .. utils.fgcolor(headercolor, "\n  Command") .. ": " .. command
+            text = text .. utils.fgcolor(headercolor, "\n  Names") .. ": " .. names
+            text = text .. utils.fgcolor(headercolor, "\n  Tags") .. ":"
+            for _, tag in pairs(tags) do
+                text = text .. "\n    " .. tag
+            end
+
+            data[#data + 1] = utils.font("monospace 10", text)
+        end
+
+        icon.tooltip:set_markup(table.concat(data, "\n\n"))
+    end
+
+    widget:connect_signal("mouse::enter", icon.tooltip.update)
+
     vicious.register(icon, workers.docker, function(widget, args)
         if #args == 0 then
             icon:set_image(beautiful.widget_docker)
         else
             icon:set_image(beautiful.widget_docker_active)
         end
-        local text = table.concat(args, "\n")
-        text = string.format(utils.font("monospace 10", "%s"), text)
-        icon.tooltip:set_markup(text)
         return ""
     end, 13)
 
--- awesome/eldritch/workers/docker.lua
+++ awesome/eldritch/workers/docker.lua
@@ -1,32 +1,10 @@
-local utils = require("eldritch.utils")
-
 local docker = {}
 
 local function worker(format, warg)
-    local f = io.popen("docker ps --format='{{.Image}}'")
+    local f = io.popen("docker ps -q")
 
     local images = {}
-
-    while true do
-        local line = f:read()
-        if line == nil then break end
-
-        if line:find(":") then
-            local data = utils.split(line, ":")
-
-            local repo = data[1]
-            local slash = repo:find("/")
-            if slash then
-                repo = repo:sub(slash + 1)
-            end
-
-            local tag = data[2]
-
-            images[#images + 1] = repo .. ":" .. tag
-        else
-            images[#images + 1] = line
-        end
-    end
+    for line in f:lines() do images[#images + 1] = line end
 
     f:close()
 
