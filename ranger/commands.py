800d62d91d782c8021092a852988ae571b2426d6
Jan Larres <jan@majutsushi.net>
2020-07-14 22:18:04 +1200

    ranger: fix custom commands

--- ranger/commands.py
+++ ranger/commands.py
@@ -1,5 +1,12 @@
-from ranger.api.commands import *
+import os
+import shutil
 import subprocess
+import sys
+from pathlib import Path
+from time import strftime
+
+from ranger.api.commands import Command
+
 
 class movetotrash(Command):
     """:movetotrash
@@ -7,53 +14,61 @@ class movetotrash(Command):
     Moves the selection or the current file to the XDG trash.
     """
 
-    def execute(self):
-        import os
-        import datetime
-        import shutil
-        from time import strftime
+    def execute(self) -> None:
+
         if self.rest(1):
-            self.fm.notify("Error: movetotrash takes no arguments! It "
-                           "operates on the selected file(s).", bad=True)
+            self.fm.notify(
+                "Error: movetotrash takes no arguments! It "
+                "operates on the selected file(s).",
+                bad=True,
+            )
             return
 
         try:
-            trash_dir = os.path.join(os.environ["XDG_DATA_HOME"], "Trash")
-        except:
-            trash_dir = os.path.join(os.environ["HOME"], ".local", "share",
-                                     "Trash")
+            trash_dir = Path(os.environ["XDG_DATA_HOME"]) / "Trash"
+        except KeyError:
+            trash_dir = Path(os.environ["HOME"]) / ".local" / "share" / "Trash"
 
         selected = self.fm.thistab.get_selection()
         self.fm.copy_buffer -= set(selected)
         if selected:
             for f in selected:
-                dest = os.path.join(trash_dir, "files", f.basename)
+                dest = trash_dir / "files" / f.basename
                 basename = f.basename
-                if os.path.exists(dest):
+                if dest.exists():
                     counter = 2
-                    while os.path.exists(dest + "." + str(counter)):
+                    while os.path.exists(os.fspath(dest) + "." + str(counter)):
                         counter += 1
                     basename += "." + str(counter)
-                with open(os.path.join(trash_dir, "info",
-                                       basename + ".trashinfo"), "w") as info:
+                with open(trash_dir / "info" / (basename + ".trashinfo"), "w") as info:
                     info.write(
-                        "[Trash Info]\n" +
-                        "Path=" + f.path + "\n" +
-                        "DeletionDate=" + strftime("%Y-%m-%dT%H:%M:%S") + "\n"
+                        "[Trash Info]\n"
+                        + f"Path={f.path}\n"
+                        + f"DeletionDate={strftime('%Y-%m-%dT%H:%M:%S')}\n"
                     )
-                shutil.move(f.path, os.path.join(trash_dir, "files", basename))
+                shutil.move(f.path, trash_dir / "files" / basename)
         self.fm.thistab.ensure_correct_pointer()
+
 
 class fzfjump(Command):
     """:fzfjump
 
     Use fzf to quickly jump to recent dirs
     """
-    def execute(self):
-        with open("/home/user/jan/.var/lib/zsh/recent-dirs") as f:
-            try:
-                directory = subprocess.check_output(["fzf", "-e", "-s", "+m"], stdin=f).strip()
-                self.fm.cd(directory)
-            except:
-                pass
+
+    def execute(self) -> None:
+        with open(Path.home() / ".var/lib/zsh/recent-dirs") as f:
+            entries = (" ".join(line.split()[3:]) for line in f)
+            unique_entries = list(dict.fromkeys(entries))
+            directory = (
+                subprocess.run(
+                    ["fzf", "-e", "+s", "+m"],
+                    check=False,
+                    input="\n".join(unique_entries).encode(sys.stdin.encoding),
+                    stdout=subprocess.PIPE,
+                )
+                .stdout.strip()
+                .decode(sys.stdout.encoding)
+            )
+            self.fm.cd(directory)
             self.fm.ui.redraw_window()
