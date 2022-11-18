d4e895b207c7c0319f0c1ae52326328b609b8103
Jan Larres <jan@majutsushi.net>
2022-11-18 19:57:03 +1300

    awesome: use bottom-tile layout by default for vertical screens

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -236,7 +236,11 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, s, awful.layout.layouts[1])
+    if s.geometry.width >= s.geometry.height then
+        awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, s, awful.layout.layouts[1])
+    else
+        awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, s, awful.layout.layouts[3])
+    end
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
