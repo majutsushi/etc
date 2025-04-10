49178240633e90e04ee14b90a4799d3b78b27e42
Jan Larres <jan@majutsushi.net>
2025-04-11 11:16:50 +1200

    yazi: use official smart-enter plugin

--- dotfiles/yazi/plugins/smart-enter.yazi/main.lua
+++
@@ -1,7 +0,0 @@
---- @sync entry
-return {
-    entry = function()
-        local h = cx.active.current.hovered
-        ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-    end,
-}
