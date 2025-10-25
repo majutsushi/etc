b2e4a6b198aa31e588b3909d1d1b78cc3b8a8c22
Jan Larres <jan@majutsushi.net>
2025-10-25 19:51:01 +1300

    sway: support pausing in inactive-windows-transparency.py

--- dotfiles/sway/inactive-windows-transparency.py
+++ dotfiles/sway/inactive-windows-transparency.py
@@ -7,8 +7,14 @@ import i3ipc
 
 OPACITY = "0.7"
 
+prev_focused = None
+paused = False
 
-def on_window_focus(ipc, event):
+
+def on_window_focus(ipc: i3ipc.Connection, event):
+    if paused:
+        return
+
     global prev_focused
 
     focused_workspace = ipc.get_tree().find_focused()
@@ -24,24 +30,43 @@ def on_window_focus(ipc, event):
         prev_focused = focused
 
 
-def reset_opacity(ipc):
+def set_opacity(ipc: i3ipc.Connection):
+    global prev_focused
+    for window in ipc.get_tree():
+        if window.focused:
+            prev_focused = window
+        else:
+            window.command("opacity " + OPACITY)
+
+
+def reset_opacity(ipc: i3ipc.Connection):
     for workspace in ipc.get_tree().workspaces():
         for w in workspace:
             w.command("opacity 1")
+
+
+def toggle_pause(ipc: i3ipc.Connection):
+    global paused
+    paused = not paused
+    if paused:
+        reset_opacity(ipc)
+    else:
+        set_opacity(ipc)
+
+
+def shutdown(ipc: i3ipc.Connection):
+    reset_opacity(ipc)
     ipc.main_quit()
     sys.exit(0)
 
 
 if __name__ == "__main__":
     ipc = i3ipc.Connection()
-    prev_focused = None
 
-    for window in ipc.get_tree():
-        if window.focused:
-            prev_focused = window
-        else:
-            window.command("opacity " + OPACITY)
     for sig in [signal.SIGINT, signal.SIGTERM]:
-        signal.signal(sig, lambda signal, frame: reset_opacity(ipc))
+        signal.signal(sig, lambda _signal, _frame: shutdown(ipc))
+    signal.signal(signal.SIGUSR1, lambda _signal, _frame: toggle_pause(ipc))
+
+    set_opacity(ipc)
     ipc.on("window::focus", on_window_focus)
     ipc.main()
