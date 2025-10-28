c093f72c1c541245914bd8fce352e972c032ae46
Jan Larres <jan@majutsushi.net>
2025-10-28 15:42:39 +1300

    yazi: implement preloading for lessfilter plugin

--- dotfiles/yazi/plugins/lessfilter.yazi/main.lua
+++ dotfiles/yazi/plugins/lessfilter.yazi/main.lua
@@ -1,39 +1,38 @@
 local M = {}
 
 function M:peek(job)
-    local dotfiles = os.getenv("DOTFILES")
-    local child = Command(dotfiles .. "/less/lessfilter")
-        :arg({
-            tostring(job.file.url),
-        })
-        :stdout(Command.PIPED)
-        :stderr(Command.PIPED)
-        :spawn()
+    -- Temporarily change 'skip' as it is used as part of the cache hash
+    -- See https://github.com/sxyazi/yazi/issues/2531
+    local offset = job.skip or 0
+    job.skip = 0
+    local cache = ya.file_cache(job)
+    job.skip = offset
+    if not cache then
+        return
+    end
 
-    if not child then
-        return self:print_error(job)
+    local ok, err = self:preload(job)
+    if not ok or err then
+        return ya.preview_widget(job, err)
     end
 
     local limit = job.area.h
     local i, lines = 0, ""
-    repeat
-        local next, event = child:read_line()
-        if event == 2 then
+    for line in io.lines(tostring(cache)) do
+        i = i + 1
+        if i > job.skip + limit then
             break
         end
-
-        i = i + 1
         if i > job.skip then
-            lines = lines .. next
+            lines = lines .. line .. "\n"
         end
-    until i >= job.skip + limit
+    end
 
-    child:start_kill()
     if job.skip > 0 and i < job.skip + limit then
-        ya.manager_emit("peek", { math.max(0, i - limit), only_if = tostring(job.file.url), upper_bound = true })
+        ya.emit("peek", { math.max(0, i - limit), only_if = job.file.url, upper_bound = true })
     else
         lines = lines:gsub("\t", string.rep(" ", rt.preview.tab_size))
-        ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area):wrap(rt.preview.wrap == "yes" and ui.Text.WRAP or ui.Text.WRAP_NO) })
+        ya.preview_widget(job, { ui.Text.parse(lines):area(job.area):wrap(rt.preview.wrap == "yes" and ui.Text.WRAP or ui.Text.WRAP_NO) })
     end
 end
 
@@ -48,14 +47,38 @@ function M:seek(job)
     end
 end
 
-function M:print_error(job)
-    p = ui.Text({
-        ui.Line {
-            ui.Span("Failed to spawn `lessfilter` command"),
-        },
-    }):area(job.area)
+function M:preload(job)
+    local offset = job.skip or 0
+    job.skip = 0
+    local cache = ya.file_cache(job)
+    job.skip = offset
+    if not cache or fs.cha(cache) then
+        return true
+    end
 
-    ya.preview_widgets(job, { p:wrap(ui.Text.WRAP) })
+    local dotfiles = os.getenv("DOTFILES")
+
+    -- stylua: ignore
+    local output, err = Command(dotfiles .. "/less/lessfilter")
+        :arg({
+            tostring(job.file.url),
+        })
+        :stdout(Command.PIPED)
+        :stderr(Command.PIPED)
+        :output()
+
+    if not output then
+        return true, Err("Failed to start `lessfilter`, error: %s", err)
+    elseif not output.status.success then
+        return true, Err("Failed to run `lessfilter`, stderr: %s", output.stderr)
+    end
+
+    local ok, err = fs.write(cache, output.stdout)
+    if ok then
+        return true
+    else
+        return false, Err("Failed to lessfilter output to `%s`, error: %s", cache, err)
+    end
 end
 
 return M
